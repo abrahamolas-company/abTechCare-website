ef0f270cd1a9ccb214508c7252981c34a520aec7
Iwumezie <chiwumezie@gmail.com>
2025-02-26 19:29:16 +0100

    feat:import images

--- public/images.ts
+++ public/images.ts
@@ -3,6 +3,24 @@ import logo_dark from './images/logo_dark.png';
 import home_hero from './images/home_hero.png';
 import book_a_repair from './images/book_a_repair.png';
 import b2b_section from './images/b2b_section.png';
+import service_section from './images/service_section.png';
+import payment_image from './images/payment_image.png';
+import insurance_image from './images/insurance_image.png';
+import tracker_image from './images/tracker_image.png';
+import about_section from './images/about_section.png';
+import globe from './images/globe.jpg';
+import home from './images/home.png';
+import speaker from './images/speaker.png';
+import drones from './images/drones.png';
+import phone from './images/phone.png';
+import laptop from './images/laptop.png';
+import game_console from './images/game_console.png';
+import frame_design from './images/frame_design.png';
+import repair_center from './images/repair_center.png';
+import pickup_delivery_image from './images/pickup_delivery_image.png';
+import insurance_bg_img from './images/insurance_bg_img.png';
+import track_your_repair from './images/Track Repair Hero.png';
+import quote_hero from './images/Quote Hero Page.png';
 
 
 
@@ -11,5 +29,23 @@ export default {
     logo_dark,
     home_hero,
     book_a_repair,
-    b2b_section
-    }
+    b2b_section,
+    service_section,
+    payment_image,
+    insurance_image,
+    tracker_image,
+    about_section,
+    globe,
+    home,
+    repair_center,
+    quote_hero,
+    speaker,
+    drones,
+    phone,
+    laptop,
+    game_console,
+    frame_design,
+    insurance_bg_img,
+    track_your_repair,
+    pickup_delivery_image
+}
