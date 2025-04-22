6954896b94bee4cf2420b1b72fcada1855dc4de6
Iwumezie <chiwumezie@gmail.com>
2025-04-22 10:55:07 +0100

    integrate password reset

--- app/reset-password/ResetPasswordPage.tsx
+++ app/reset-password/ResetPasswordPage.tsx
@@ -5,9 +5,65 @@ import { sectionPadding } from '@/app/styles/styles'
 import images from '@/public/images'
 import Image from 'next/image'
 import Link from 'next/link'
-import React  from 'react'
+import React, { useState } from 'react'
+import { useResetPassword } from '../api/apiClient'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { ResetPasswordRequest } from '../components/models/IPassword'
+import { toast } from 'sonner'
+import { catchError } from '../components/constants/catchError'
 
 function ResetPasswordPage() {
+
+    const resetPassword = useResetPassword()
+
+    const searchParams = useSearchParams();
+
+    const router = useRouter()
+
+    const token = searchParams.get('token');
+
+    const email = searchParams.get('email');
+    const [isLoading, setIsLoading] = useState(false)
+
+    const [formValues, setFormValues] = useState<ResetPasswordRequest>({ password: '' } as ResetPasswordRequest);
+
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        // Prevent the default form submission
+        e.preventDefault();
+        setError(''); // Clear any previous errors
+
+        if (formValues.password !== confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+
+        // Start loader
+        setIsLoading(true)
+
+        const data: ResetPasswordRequest = {
+            password: formValues?.password as string,
+            token: token as string,
+            email: email as string
+        }
+
+        await resetPassword(data)
+            .then((response) => {
+                setFormValues({ password: '' } as ResetPasswordRequest);
+                toast.success('Password reset successfully');
+                router.push('/login')
+            })
+            .catch((error) => {
+                catchError(error)
+                toast.error('An error occurred. Please try again.');
+            })
+            .finally(() => {
+                // Close loader
+                setIsLoading(false);
+            });
+    }
 
     return (
         <section className={`${sectionPadding} pb-24`}>
@@ -19,17 +75,36 @@ function ResetPasswordPage() {
                 Create a New password to Login
             </p>
 
-            <form className='w-fit mx-auto flex flex-col'>
+            <form className='w-fit mx-auto flex flex-col' onSubmit={(e) => handleSubmit(e)}>
                 <div className="mb-7">
-                    <Label >New Password</Label>
-                    <Input className='!mt-1' placeholder='Enter your New Password ' />
+                    <Label htmlFor='password' >New Password</Label>
+                    <Input
+                        name='password'
+                        id="password"
+                        className='!mt-1' placeholder='Enter your New Password '
+                        onChange={(e) => setFormValues({
+                            ...formValues, password: e.target.value
+                        } as ResetPasswordRequest
+                        )}
+                    />
                 </div>
                 <div className="mb-5">
-                    <Label >Confirm  Password</Label>
-                    <Input className='!mt-1' placeholder='Confirm New Password' />
+                    <Label htmlFor='confirmPassword'>Confirm  Password</Label>
+                    <Input
+                        name='confirmPassword'
+                        id="confirmPassword"
+                        className='!mt-1'
+                        placeholder='Confirm New Password'
+                        onChange={(e) => {
+                            setConfirmPassword(e.target.value)
+                            setError('')
+                        }}
+                    />
+
+                    {error && <span className='text-red-500 text-sm'>{error}</span>}
                 </div>
 
-                <button className="bg-[#FFCC29] font-medium mb-1 flex items-center justify-center mx-auto text-sm rounded-lg text-[#211D1D] py-3 min-w-[200px] transition-all ease-in-out duration-300 border border-[#FFCC29] hover:bg-transparent hover:text-[#211D1D]">
+                <button type='submit' disabled={isLoading} className={`bg-[#FFCC29] font-medium mb-1 flex items-center justify-center mx-auto text-sm rounded-lg text-[#211D1D] py-3 min-w-[200px] transition-all ease-in-out duration-300 border border-[#FFCC29] hover:bg-transparent hover:text-[#211D1D] disabled:pointer-events-none disabled:opacity-60`}>
                     Reset Password
                 </button>
             </form>
